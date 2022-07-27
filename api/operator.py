8b03c0f52aaf50e5ec4c79aaddb7a58f0f46c47b
MaxChangInnodisk <max_chang@innodisk.com>
2022-07-27 10:18:17 +0800

    feat: define general function in __init__.py and import it in other module

--- api/operator.py
+++ api/operator.py
@@ -3,6 +3,8 @@ import subprocess as sb
 import logging, copy, sys, os
 from flask import Blueprint, abort, jsonify, current_app, request
 
+from . import get_request_data, print_title
+
 from ..tools.handler import edit_task, add_task, get_tasks, remove_task, import_task
 from ..tools.parser import get_pure_jsonify
 from ..tools.common import handle_exception
@@ -10,17 +12,24 @@ from ..tools.common import handle_exception
 from werkzeug.utils import secure_filename
 from flasgger import swag_from
 
-# Define API Docs path
+# Define API Docs path and Blue Print
 YAML_PATH       = "/workspace/ivit_i/web/docs/operator"
+BP_NAME         = "operator"
+bp_operators = Blueprint(BP_NAME, __name__)
 
 # Define app config key
 AF              = "AF"
 TASK_LIST       = "TASK_LIST"
 DATA            = "DATA"
 TEMP_PATH       = "TEMP_PATH"
+INFO            = "info"
+TRT             = "tensorrt" 
+
+# Define key for Subprocess
 PROC            = "proc"
 IMPORT_PROC     = "IMPORT_PROC"
-TRT             = "tensorrt" 
+PROC_RUN        = "running"
+PROC_DONE       = "done"
 
 # Define key for request data
 FRAMEWORK_KEY   = "framework"
@@ -28,86 +37,29 @@ SOURCE_KEY      = "source"
 THRES_KEY       = "thres"
 
 # Define key for ZIP file from iVIT-T
-LABEL_NAME      = "classes"
-CLS             = "cls"
-OBJ             = "obj"
-DARKNET         = "darknet"
+LABEL_NAME          = "classes"
+CLS                 = "cls"
+OBJ                 = "obj"
+DARKNET             = "darknet"
 CLASSIFICATION_KEY  = "classification"
 YOLO_KEY            = "yolo"
 
 # Define extension for ZIP file form iVIT-T
 DARK_LABEL_EXT  = CLS_LABEL_EXT = ".txt"        # txt is the category list
 DARK_JSON_EXT   = CLS_JSON_EXT  = ".json"       # json is for basic information like input_shape, preprocess
-
-## Darknet format for tensorrt
 DARK_MODEL_EXT  = ".weights"    
 DARK_CFG_EXT    = ".cfg"
-
-## onnx format for tensorrt
 CLS_MODEL_EXT   = ".onnx"
-
-## ir model for openvino
 IR_MODEL_EXT    = ".xml"
 
-bp_operators = Blueprint('operator', __name__)
-
-def get_request_file(save_file=False):
-    """
-    Get request file
-     - Argument
-        - save_file
-            - type: bool
-            - desc: set True if need to save file
-     - Output
-        - file name/path
-            - type: String
-            - desc: return file path if save_file is True, on the other hand, return name
-    """
-    
-    file        = request.files[SOURCE_KEY]
-    file_name   = secure_filename(file.filename)
-    
-    if save_file:
-        try:
-            file_path = os.path.join(current_app.config[DATA], secure_filename(file_name))
-            file.save( file_path )
-        except Exception as e:
-            err = handle_exception(e, "Error when saving file ...")
-            abort(404, {'message': err } )
-
-        return file_path
-    
-    return file_name
-
-def get_request_data():
-    """ Get data form request and parse content. """
-    # Support form data and json
-    data = dict(request.form) if bool(request.form) else request.get_json()
-
-    # Put framework information into data
-    if FRAMEWORK_KEY not in data.keys(): 
-        data.update( { FRAMEWORK_KEY : current_app.config['AF'] } )
-
-    # Source: If got new source
-    if bool(request.files):
-        file_path = get_request_file(save_file=True)
-        data[SOURCE_KEY] = file_path
-        logging.debug("Get data ({})".format(data[SOURCE_KEY]))
-        
-    # Set the format of thres to float
-    data[THRES_KEY]=float( data[THRES_KEY] )
-    
-    # Print out to check information
-    print_data(data)
-
-    return data
-
-def print_title(title):
-    logging.info( "{}\n{}".format('-' * 3, title) )
-
-def print_data(data, title='Check request data'):
-    logging.debug(title)
-    [ logging.debug(" - {}: {}".format(key, data)) for key, val in data.items() ]
+# Return Pattern when ZIP file is extracted
+NAME            = "name"
+PATH            = "path"
+TAG             = "tag"
+MODEL_PATH      = "model_path"
+LABEL_PATH      = "label_path"
+CONFIG_PATH     = "config_path"
+JSON_PATH       = "json_path"
 
 @bp_operators.after_request
 def after_request(response):
@@ -244,7 +196,7 @@ def import_extract_event():
 
     # It have to convert model if the framework is tensorrt
     convert_proc = None
-    if current_app.config["AF"]=="tensorrt":
+    if current_app.config[AF]==TRT:
         logging.warning("It have to convert model if the framework is tensorrt")
         
         # capture model name with path which without the extension
@@ -272,19 +224,19 @@ def import_extract_event():
         current_app.config.update( {key:dict()})
     if not ( task_name in current_app.config[key] ):
         current_app.config[key].update( { task_name:dict() })
-    current_app.config[key][task_name]["proc"]=convert_proc
+    current_app.config[key][task_name][PROC]=convert_proc
 
     # return information
     ret = {
-        "name": task_name,
-        "path": task_path,
-        "tag": trg_tag,
-        "model_path": trg_model_path,
-        "label_path": trg_label_path,
-        "config_path": trg_cfg_path,
-        "json_path": trg_json_path
+        NAME        : task_name,
+        PATH        : task_path,
+        TAG         : trg_tag,
+        MODEL_PATH  : trg_model_path,
+        LABEL_PATH  : trg_label_path,
+        CONFIG_PATH : trg_cfg_path,
+        JSON_PATH   : trg_json_path
     }
-    current_app.config[key][task_name]["info"]=ret    
+    current_app.config[key][task_name][INFO]=ret    
 
     logging.debug(ret)
 
@@ -299,15 +251,17 @@ def import_process_default_event():
 @bp_operators.route("/import_proc/<task_name>/status", methods=["GET"])
 @swag_from("{}/{}".format(YAML_PATH, "import_proc_status.yml"))
 def import_process_event(task_name):
+    
     proc = current_app.config[IMPORT_PROC][task_name][PROC]
+
     if proc!=None:
         try:
-            ret = "running" if proc.poll() is None else "done"
+            ret = PROC_RUN if proc.poll() is None else PROC_DONE
         except Exception as e:
             handle_exception(e)
-            ret = "done"
+            ret = PROC_DONE
     else:
-        ret = "done"
+        ret = PROC_DONE
     return jsonify( ret )
 
 @bp_operators.route("/import_2/", methods=["POST"])
