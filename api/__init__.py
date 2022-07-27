da53469f563da222e147652eecfbea2a522a445e
MaxChangInnodisk <max_chang@innodisk.com>
2022-07-27 10:18:09 +0800

    feat: define general function in __init__.py and import it in other module

--- api/__init__.py
+++ api/__init__.py
@@ -1,139 +1,297 @@
-# import logging, os
-# from flask import abort, current_app, request
-# import cv2, time, logging, base64, threading, os, sys, copy, json
+import logging, os
+from flask import abort, current_app, request
+import cv2, time, logging, base64, threading, os, sys, copy, json
 
-# from ..tools.common import handle_exception
+from ..tools.common import handle_exception
+from ..ai.pipeline import Source
+from ..tools.common import handle_exception
 
-# from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename
 
-# # Define app config key
-# AF              = "AF"
-# TASK_LIST       = "TASK_LIST"
-# DATA            = "DATA"
-# TEMP_PATH       = "TEMP_PATH"
-# PROC            = "proc"
-# IMPORT_PROC     = "IMPORT_PROC"
-# TRT             = "tensorrt" 
+# Define app config key
+AF              = "AF"
+TASK_LIST       = "TASK_LIST"
+DATA            = "DATA"
+TEMP_PATH       = "TEMP_PATH"
+PROC            = "proc"
+IMPORT_PROC     = "IMPORT_PROC"
+TRT             = "tensorrt" 
 
-# # Define key for request data
-# FRAMEWORK_KEY   = "framework"
-# SOURCE_KEY      = "source"
-# THRES_KEY       = "thres"
+# Define key for request data
+FRAMEWORK_KEY   = "framework"
+SOURCE_KEY      = "source"
+THRES_KEY       = "thres"
 
-# # Define key for ZIP file from iVIT-T
-# LABEL_NAME      = "classes"
-# CLS             = "cls"
-# OBJ             = "obj"
-# DARKNET         = "darknet"
-# CLASSIFICATION_KEY  = "classification"
-# YOLO_KEY            = "yolo"
+# Define key for ZIP file from iVIT-T
+LABEL_NAME      = "classes"
+CLS             = "cls"
+OBJ             = "obj"
+DARKNET         = "darknet"
+CLASSIFICATION_KEY  = "classification"
+YOLO_KEY            = "yolo"
 
-# # Define extension for ZIP file form iVIT-T
-# DARK_LABEL_EXT  = CLS_LABEL_EXT = ".txt"        # txt is the category list
-# DARK_JSON_EXT   = CLS_JSON_EXT  = ".json"       # json is for basic information like input_shape, preprocess
+# Define extension for ZIP file form iVIT-T
+DARK_LABEL_EXT  = CLS_LABEL_EXT = ".txt"        # txt is the category list
+DARK_JSON_EXT   = CLS_JSON_EXT  = ".json"       # json is for basic information like input_shape, preprocess
 
-# ## Darknet format for tensorrt
-# DARK_MODEL_EXT  = ".weights"    
-# DARK_CFG_EXT    = ".cfg"
+## Darknet format for tensorrt
+DARK_MODEL_EXT  = ".weights"    
+DARK_CFG_EXT    = ".cfg"
 
-# ## onnx format for tensorrt
-# CLS_MODEL_EXT   = ".onnx"
+## onnx format for tensorrt
+CLS_MODEL_EXT   = ".onnx"
 
-# ## ir model for openvino
-# IR_MODEL_EXT    = ".xml"
+## ir model for openvino
+IR_MODEL_EXT    = ".xml"
 
-# def get_request_file(save_file=False):
-#     """
-#     Get request file
-#      - Argument
-#         - save_file
-#             - type: bool
-#             - desc: set True if need to save file
-#      - Output
-#         - file name/path
-#             - type: String
-#             - desc: return file path if save_file is True, on the other hand, return name
-#     """
+# Define Status
+RUN         = "run"
+STOP        = "stop"
+ERROR       = "error"
+STATUS      = "status"
+
+# Define Key which in app.config
+TASK        = "TASK"
+TASK_LIST   = "TASK_LIST"
+UUID        = "UUID"
+TAG         = "tag"
+
+# Define Key which declared in app.config[SRC]
+SRC         = "SRC"
+OBJECT      = "object"
+TYPE        = "type"
+PROC        = "proc"
+
+# Define Key which declared in each task
+FRAMEWORK   = "framework"
+CONFIG      = "config"
+LABEL_PATH  = "label_path"
+APPLICATION = "application"
+MODEL       = "model"
+NAME        = "name"
+SOURCE      = "source"
+SOURCE_TYPE = "source_type"
+DEVICE      = "device"
+STATUS      = "status"
+THRES       = "thres"
+START_TIME  = "start_time"
+DETS        = "detections"
+
+
+# Define AI Inference Parameters
+API         = "api"
+RUNTIME     = "runtime"
+DRAW_TOOLS  = "draw_tools"
+PALETTE     = "palette"
+FRAME_IDX   = "frame_index"
+STREAM      = "stream"
+
+# Define SocketIO Event
+IMG_EVENT   = "images"
+RES_EVENT   = "results"
+
+# Define SocketIO Parameters
+IDX         = "idx"
+INFER       = "inference"
+FPS         = "fps"
+FIRST_TIME  = "first_time_flag"
+LIVE_TIME   = "live_time"
+G_TEMP      = "gpu_temp"
+G_LOAD      = "gpu_load"
+
+RET_INFO = {
+    IDX         : None,
+    DETS        : None,
+    INFER       : None,
+    FPS         : None,
+    LIVE_TIME   : None,
+    G_TEMP      : None,
+    G_LOAD      : None,
+}
+
+# Define Stream Type
+RTSP        = 'rtsp'
+VIDEO       = 'video'
+BASE64_EXT  = '.jpg'
+BASE64_DEC  = "utf-8"
+PASS_CODE   = 200
+FAIL_CODE   = 400
+
+# Brand & Framework Info
+NV          = "nvidia"
+TRT         = "tensorrt"
+INTEL       = "intel"
+OV          = "openvino"
+XLNX        = "xilinx"
+VTS         = "vitis-ai"
+
+def get_request_file(save_file=False):
+    """
+    Get request file
+     - Argument
+        - save_file
+            - type: bool
+            - desc: set True if need to save file
+     - Output
+        - file name/path
+            - type: String
+            - desc: return file path if save_file is True, on the other hand, return name
+    """
     
-#     file        = request.files[SOURCE_KEY]
-#     file_name   = secure_filename(file.filename)
+    file        = request.files[SOURCE_KEY]
+    file_name   = secure_filename(file.filename)
     
-#     if save_file:
-#         try:
-#             file_path = os.path.join(current_app.config[DATA], secure_filename(file_name))
-#             file.save( file_path )
-#         except Exception as e:
-#             err = handle_exception(e, "Error when saving file ...")
-#             abort(404, {'message': err } )
+    if save_file:
+        try:
+            file_path = os.path.join(current_app.config[DATA], secure_filename(file_name))
+            file.save( file_path )
+        except Exception as e:
+            err = handle_exception(e, "Error when saving file ...")
+            abort(404, {'message': err } )
 
-#         return file_path
+        return file_path
     
-#     return file_name
+    return file_name
 
-# def get_request_data():
-#     """ Get data form request and parse content. """
-#     # Support form data and json
-#     data = dict(request.form) if bool(request.form) else request.get_json()
+def get_request_data():
+    """ Get data form request and parse content. """
+    # Support form data and json
+    data = dict(request.form) if bool(request.form) else request.get_json()
 
-#     # Put framework information into data
-#     if FRAMEWORK_KEY not in data.keys(): 
-#         data.update( { FRAMEWORK_KEY : current_app.config['AF'] } )
+    # Put framework information into data
+    if FRAMEWORK_KEY not in data.keys(): 
+        data.update( { FRAMEWORK_KEY : current_app.config['AF'] } )
 
-#     # Source: If got new source
-#     if bool(request.files):
-#         file_path = get_request_file(save_file=True)
-#         data[SOURCE_KEY] = file_path
-#         logging.debug("Get data ({})".format(data[SOURCE_KEY]))
+    # Source: If got new source
+    if bool(request.files):
+        file_path = get_request_file(save_file=True)
+        data[SOURCE_KEY] = file_path
+        logging.debug("Get data ({})".format(data[SOURCE_KEY]))
         
-#     # Set the format of thres to float
-#     data[THRES_KEY]=float( data[THRES_KEY] )
+    # Set the format of thres to float
+    data[THRES_KEY]=float( data[THRES_KEY] )
     
-#     # Print out to check information
-#     print_data(data)
+    # Print out to check information
+    print_data(data)
 
-#     return data
+    return data
 
-# def print_title(title):
-#     logging.info( "{}\n{}".format('-' * 3, title) )
+def print_title(title):
+    logging.info( "{}\n{}".format('-' * 3, title) )
 
-# def print_data(data, title='Check request data'):
-#     logging.debug(title)
-#     [ logging.debug(" - {}: {}".format(key, data)) for key, val in data.items() ]
+def print_data(data, title='Check request data'):
+    logging.debug(title)
+    [ logging.debug(" - {}: {}".format(key, data)) for key, val in data.items() ]
 
+def frame2btye(frame):
+    """
+    Convert the image with numpy array format to btye ( base64 )
 
-# def frame2btye(frame):
-#     """
-#     Convert the image with numpy array format to btye ( base64 )
+    - Arguments
+        - frame
+            - type: numpy.array
+    - Output
+        - ret
+            - type: dict
+            - parameters
+                - image
+                    - type: btye
+                    - desc: the image which format is btye
+                - height
+                    - type: int
+                    - desc: the orginal image height
+                - width
+                    - type: int
+                    - desc: the orginal image width
+                - channel
+                    - type: int
+                    - desc: the orginal image channel
+    """
+    ret, (h,w,c) = None, frame.shape
+    frame_base64 = base64.encodebytes(
+        cv2.imencode('.jpg', frame)[1].tobytes()
+    ).decode("utf-8")
 
-#     - Arguments
-#         - frame
-#             - type: numpy.array
-#     - Output
-#         - ret
-#             - type: dict
-#             - parameters
-#                 - image
-#                     - type: btye
-#                     - desc: the image which format is btye
-#                 - height
-#                     - type: int
-#                     - desc: the orginal image height
-#                 - width
-#                     - type: int
-#                     - desc: the orginal image width
-#                 - channel
-#                     - type: int
-#                     - desc: the orginal image channel
-#     """
-#     ret, (h,w,c) = None, frame.shape
-#     frame_base64 = base64.encodebytes(
-#         cv2.imencode('.jpg', frame)[1].tobytes()
-#     ).decode("utf-8")
+    ret = {
+        "image"     : frame_base64,
+        "height"    : h,
+        "width"     : w,
+        "channel"   : c
+    }
+    return ret
 
-#     ret = {
-#         "image"     : frame_base64,
-#         "height"    : h,
-#         "width"     : w,
-#         "channel"   : c
-#     }
-#     return ret
+def get_src(task_uuid, reload_src=False):
+    """ 
+    Setup the source object and run 
+
+    - Arguments
+        - task_uuid
+            - type: string
+        - reload_src
+            - type: bool
+            - desc: reset the source object
+    - Output
+        - source_object
+            - type: object
+            - desc: the source object same with app.config["SRC"][{src_name}][object]
+    """
+
+    # get the target source name
+    src_name = current_app.config[TASK][task_uuid][SOURCE]
+    
+    # if source is None or reload_src==True then create a new source
+    if ( current_app.config[SRC][src_name][OBJECT] == None ) or reload_src:
+        logging.info('Initialize a new source.')
+        current_app.config[SRC][src_name][OBJECT] = Source(src_name, current_app.config[SRC][src_name][TYPE])
+    
+    # setup status and error message in source config
+    status, err = current_app.config[SRC][src_name][OBJECT].get_status()
+    current_app.config[SRC][src_name][STATUS], current_app.config[SRC][src_name][ERROR] = RUN if status else ERROR, err
+    
+    # return object
+    return current_app.config[SRC][src_name][OBJECT]
+
+def stop_src(task_uuid, release=False):
+    """ 
+    Stop the source and release it if needed, but if the source still be accesed by process, then it won't be stopped. 
+    
+    - Argument
+        - task_uuid
+            - type: string
+        - release
+            - type: bool
+            - desc: release the source object
+    """
+    
+    # initialize
+    src_name, stop_flag, access_proc = current_app.config[TASK][task_uuid][SOURCE], True, []
+    
+    # checking all process
+    for _uuid in current_app.config[SRC][src_name][PROC]:
+        
+        if _uuid in current_app.config[TASK].keys():
+            
+            # if any task with same source is still running, then set stop_flag to False
+            if ( task_uuid!=_uuid) and (current_app.config[TASK][_uuid][STATUS]==RUN): 
+                access_proc.append(_uuid)
+                stop_flag = False           
+        else:
+            # clean unused uuid
+            current_app.config[SRC][src_name][PROC].remove(_uuid)
+    
+    # clear source object
+    if stop_flag:
+        logging.info('Stopping source object ...')
+        current_app.config[SRC][src_name][STATUS] = STOP
+
+        if current_app.config[SRC][src_name][OBJECT] != None: 
+            # need release source
+            if release:
+                logging.warning('Release source ...')
+                current_app.config[SRC][src_name][OBJECT].release() 
+                current_app.config[SRC][src_name][OBJECT] = None 
+            else:
+                current_app.config[SRC][src_name][OBJECT].stop()
+        logging.info('Stop the source.')
+    else:
+        logging.warning("Stop failed, source ({}) accessed by {} ".format(src_name, access_proc))
--- api/stream.py
+++ api/stream.py
@@ -3,6 +3,8 @@ import cv2, time, logging, base64, threading, os, sys, copy, json
 from flask import Blueprint, abort, jsonify, app, request
 from werkzeug.utils import secure_filename
 
+from . import frame2btye, get_src, stop_src
+
 from .. import socketio, app
 from ..tools.handler import get_tasks
 from ..tools.parser import get_pure_jsonify
@@ -16,6 +18,7 @@ from ivit_i.app.handler import get_application
 YAML_PATH   = ""
 BP_NAME     = "stream"
 DIV         = "-" * 20
+bp_stream = Blueprint(BP_NAME, __name__)
 
 # Define Status
 RUN         = "run"
@@ -97,121 +100,6 @@ INTEL       = "intel"
 OV          = "openvino"
 XLNX        = "xilinx"
 VTS         = "vitis-ai"
-
-bp_stream = Blueprint(BP_NAME, __name__)
-
-def frame2btye(frame):
-    """
-    Convert the image with numpy array format to btye ( base64 )
-
-    - Arguments
-        - frame
-            - type: numpy.array
-    - Output
-        - ret
-            - type: dict
-            - parameters
-                - image
-                    - type: btye
-                    - desc: the image which format is btye
-                - height
-                    - type: int
-                    - desc: the orginal image height
-                - width
-                    - type: int
-                    - desc: the orginal image width
-                - channel
-                    - type: int
-                    - desc: the orginal image channel
-    """
-    ret, (h,w,c) = None, frame.shape
-    frame_base64 = base64.encodebytes(
-        cv2.imencode('.jpg', frame)[1].tobytes()
-    ).decode("utf-8")
-
-    ret = {
-        "image"     : frame_base64,
-        "height"    : h,
-        "width"     : w,
-        "channel"   : c
-    }
-    return ret
-
-def get_src(task_uuid, reload_src=False):
-    """ 
-    Setup the source object and run 
-
-    - Arguments
-        - task_uuid
-            - type: string
-        - reload_src
-            - type: bool
-            - desc: reset the source object
-    - Output
-        - source_object
-            - type: object
-            - desc: the source object same with app.config["SRC"][{src_name}][object]
-    """
-
-    # get the target source name
-    src_name = app.config[TASK][task_uuid][SOURCE]
-    
-    # if source is None or reload_src==True then create a new source
-    if ( app.config[SRC][src_name][OBJECT] == None ) or reload_src:
-        logging.info('Initialize a new source.')
-        app.config[SRC][src_name][OBJECT] = Source(src_name, app.config[SRC][src_name][TYPE])
-    
-    # setup status and error message in source config
-    status, err = app.config[SRC][src_name][OBJECT].get_status()
-    app.config[SRC][src_name][STATUS], app.config[SRC][src_name][ERROR] = RUN if status else ERROR, err
-    
-    # return object
-    return app.config[SRC][src_name][OBJECT]
-
-def stop_src(task_uuid, release=False):
-    """ 
-    Stop the source and release it if needed, but if the source still be accesed by process, then it won't be stopped. 
-    
-    - Argument
-        - task_uuid
-            - type: string
-        - release
-            - type: bool
-            - desc: release the source object
-    """
-    
-    # initialize
-    src_name, stop_flag, access_proc = app.config[TASK][task_uuid][SOURCE], True, []
-    
-    # checking all process
-    for _uuid in app.config[SRC][src_name][PROC]:
-        
-        if _uuid in app.config[TASK].keys():
-            
-            # if any task with same source is still running, then set stop_flag to False
-            if ( task_uuid!=_uuid) and (app.config[TASK][_uuid][STATUS]==RUN): 
-                access_proc.append(_uuid)
-                stop_flag = False           
-        else:
-            # clean unused uuid
-            app.config[SRC][src_name][PROC].remove(_uuid)
-    
-    # clear source object
-    if stop_flag:
-        logging.info('Stopping source object ...')
-        app.config[SRC][src_name][STATUS] = STOP
-
-        if app.config[SRC][src_name][OBJECT] != None: 
-            # need release source
-            if release:
-                logging.warning('Release source ...')
-                app.config[SRC][src_name][OBJECT].release() 
-                app.config[SRC][src_name][OBJECT] = None 
-            else:
-                app.config[SRC][src_name][OBJECT].stop()
-        logging.info('Stop the source.')
-    else:
-        logging.warning("Stop failed, source ({}) accessed by {} ".format(src_name, access_proc))
 
 def stream_task(task_uuid, src, namespace, infer_function):
     '''
@@ -355,100 +243,6 @@ def get_first_frame(uuid):
     ret = frame2btye(src.get_first_frame())
     # return '<img src="data:image/jpeg;base64,{}">'.format(frame_base64)
     return jsonify( ret )
-
-@bp_stream.route("/task/<uuid>/run/", methods=["GET"])
-def run_task(uuid):
-    
-    # check if the task is ready to inference
-    if app.config[TASK][uuid][STATUS] == ERROR:
-        msg  = 'The task is not ready... {}'.format( app.config[TASK][uuid][ERROR])
-        return msg, FAIL_CODE
-
-    if app.config[TASK][uuid][STATUS] == RUN:
-        msg  = 'The task is still running ... '
-        code = 200
-        return msg, PASS_CODE
-    
-    # create a source object if it is not exist
-    src = get_src(uuid)
-    src_status, src_err = src.get_status()
-    if not src_status:
-        logging.error('get source error')
-        app.config[TASK][uuid][ERROR] = src_err
-        return src_err, PASS_CODE
-    
-    # avoid changing the configuration data during initailization ( init)
-    temp_config = copy.deepcopy(app.config[TASK][uuid][CONFIG]) 
-    
-    # get ai objects
-    init, _ = get_api()
-    
-    # only pose estimation in openvino have to input a frame
-    is_openvino = (app.config[TASK][uuid][FRAMEWORK]==OV)
-    is_pose = (app.config[TASK][uuid][CONFIG][TAG]=='pose')
-    input_frame = src.get_frame()[1] if is_openvino and is_pose else None
-        
-    ai_objects = init(temp_config, input_frame)
-    
-    # if no object then return error message
-    if None in ai_objects:
-        msg = '{}\n( {} )'.format( ai_objects[0], "Auto restart the service" )    
-        logging.critical(msg)
-        return msg, FAIL_CODE
-    else:
-        (   app.config[TASK][uuid][API], 
-            app.config[TASK][uuid][RUNTIME], 
-            app.config[TASK][uuid][DRAW_TOOLS], 
-            app.config[TASK][uuid][PALETTE]  ) = ai_objects
-    
-    # send socketio and update app.config
-    app.config[TASK][uuid][STATUS] = RUN
-    
-    # set initialize time
-    app.config[TASK][uuid][START_TIME]  = time.time()
-    app.config[TASK][uuid][LIVE_TIME]   = 0
-    app.config[TASK][uuid][FIRST_TIME]  = True
-    
-    # set frame information
-    app.config[TASK][uuid][FRAME_IDX]   = 0
-
-    # update list
-    app.config[TASK_LIST]=get_tasks()
-
-    msg = 'Run Application ({}) !'.format(uuid)
-    return jsonify(msg), PASS_CODE
-
-@bp_stream.route("/task/<uuid>/stop/", methods=["GET"])
-def stop_task(uuid):
-    """ 
-    Stop the task: release source, set relative object to None, set task status to stop, reload task list 
-    """
-    try:
-        logging.info("Stopping task ...")
-        
-        # stop source and release source
-        stop_src(uuid, release=True)
-        
-        # set relative object to None
-        for key in [API, RUNTIME, DRAW_TOOLS, PALETTE, STREAM]:
-            app.config[TASK][uuid][key] = None
-            logging.debug(" - setting app.config[TASK][{}][{}] to None".format(
-                uuid,
-                key ))
-        
-        # set the status of task to STOP
-        app.config[TASK][uuid][STATUS] = STOP
-
-        # update list
-        app.config["TASK_LIST"] = get_tasks()
-        
-        # msg
-        msg = f"Stop the task ({uuid})"
-        logging.info( msg )
-        return jsonify( msg ), PASS_CODE
-
-    except Exception as e:
-        return jsonify(handle_exception(e)), FAIL_CODE
     
 @bp_stream.route("/task/<uuid>/stream/start/", methods=["GET"])
 def start_stream(uuid):      
@@ -464,17 +258,19 @@ def start_stream(uuid):
             name=f"{uuid}",
         )
         app.config[TASK][uuid][STREAM].daemon = True
+
     # check if thread is alive
     if app.config[TASK][uuid][STREAM].is_alive():
         logging.info('Stream is running')
         return jsonify('Stream is running'), 200
+
     try:
         app.config[TASK][uuid][STREAM].start()
         logging.info('Stream is created')
-        return jsonify('Stream is created, The results is display in /task/<uuid>/stream'), 200
+        return jsonify('Stream is created, The results is display in /task/<uuid>/stream'), PASS_CODE
+
     except Exception as e:
-        logging.error('Start thread error: {}'.format(e))
-        return jsonify('Start thread error: {}'.format(e)), 400
+        return jsonify(handle_exception(e, "Stream Start Error")), FAIL_CODE
 
 @bp_stream.route("/task/<uuid>/stream/stop/", methods=["GET"])
 def stop_stream(uuid):
