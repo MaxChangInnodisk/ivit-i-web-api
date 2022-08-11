7bdbca82832c24ff37c065f6aff25e5ca1b4b978
MaxChangInnodisk <max_chang@innodisk.com>
2022-08-11 15:34:44 +0800

    feat: auto detect the config of env ( /workspace/ivit-i.json )

--- __init__.py
+++ __init__.py
@@ -22,21 +22,25 @@ from .tools.logger import config_logger
 from .tools.common import get_address
 from .api.config import config
 
+ENV_CONF_KEY = "IVIT_I"
+ENV_CONF = "/workspace/ivit-i.json"
+
 def initialize_flask_app():
-    """
-    Initailize Flask App and Get SocketIO Object
-    ---
-    Return
+    """ Initailize Flask App and Get SocketIO Object
+    - Return
         - app
         - socketio
     """
     
     # check IVIT_I is in environment
-    if not ('IVIT_I' in os.environ.keys()):
-        raise KeyError("Could not find the environ \"IVIT_I\", please setup the custom setting path: $ export IVIT_I=/workspace/ivit-i.json")
+    if not (ENV_CONF_KEY in os.environ.keys()):
+        if os.path.exists(ENV_CONF):
+            os.environ[ENV_CONF_KEY]=ENV_CONF
+        else:
+            raise KeyError("Could not find the environ \"IVIT_I\", please setup the custom setting path: $ export IVIT_I=/workspace/ivit-i.json")
 
     # initialize logger
-    with open( os.environ["IVIT_I"], 'r' ) as f:
+    with open( os.environ[ENV_CONF_KEY], 'r' ) as f:
         data = json.load(f)
         config_logger(log_name=data["LOGGER"], write_mode='a', level='debug', clear_log=True)
 
@@ -45,7 +49,7 @@ def initialize_flask_app():
 
     # loading flask configuration
     app.config.from_object(config)
-    app.config.from_file( os.environ["IVIT_I"], load=json.load )
+    app.config.from_file( os.environ[ENV_CONF_KEY], load=json.load )
 
     # update ip address
     if app.config['HOST'] == "":
