ebc9327820f0b6dad827f8939776b7b5b69b2a1b
MaxChangInnodisk <max_chang@innodisk.com>
2022-11-17 15:44:14 +0800

    feat: remove inference function

--- ai/get_api.py
+++ ai/get_api.py
@@ -5,38 +5,32 @@ def get_api(af=None):
     """
     import the custom ai module for web api
     """
-    init, do_inference = None, None
+    init = None
     trg_af = current_app.config['AF'] if af==None else af
     logging.info("Loading AI API ({}) ...".format(trg_af))
     if trg_af in ['tensorrt', 'TensorRT', 'trt']:
         try:
             from ivit_i.web.ai.tensorrt import trt_init 
-            from ivit_i.web.ai.tensorrt import trt_inference 
             init = trt_init
-            do_inference = trt_inference
         except Exception as e:
             raise Exception(e)
 
     elif trg_af in ['openvino', 'OpenVINO', 'vino']:
         try:
             from ivit_i.web.ai.openvino import vino_init
-            from ivit_i.web.ai.openvino import vino_inference 
             init = vino_init
-            do_inference = vino_inference
         except Exception as e:
             raise Exception(e)
 
     elif trg_af in ['vitis-ai', 'Vitis-ai', 'Vitis', 'vitis']:
         try:
             from ivit_i.web.ai.vitis import vitis_init
-            from ivit_i.web.ai.vitis import vitis_inference 
             init = vitis_init
-            do_inference = vitis_inference
         except Exception as e:
             raise Exception(e)
     
-    if None in [init, do_inference]:
+    if None in [init]:
         msg = "Could not import framework API, please check the script in ivit_i.web.ai"
         logging.error(msg)
         raise Exception(msg)
-    return init, do_inference
+    return init
