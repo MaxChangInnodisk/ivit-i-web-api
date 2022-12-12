daac2ffeb1ee4d50d4b4bd3502c93f07bba95084
MaxChangInnodisk <max_chang@innodisk.com>
2022-12-11 17:32:45 -0800

    fix: import from the absolute path

--- api/common.py
+++ api/common.py
@@ -2,7 +2,7 @@ from flask import abort, request
 import cv2, time, logging, base64, threading, os, sys, copy, json
 from werkzeug.utils import secure_filename
 
-from .. import sock, app
+from ivit_i.web import sock, app
 from ivit_i.common.pipeline import Source, Pipeline
 from ivit_i.utils.err_handler import handle_exception
 
