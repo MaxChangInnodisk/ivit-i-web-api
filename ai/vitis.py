335f2d2329eb88d8607eac8826c0c25623618228
MaxChangInnodisk <max_chang@innodisk.com>
2022-12-11 17:33:16 -0800

    fix: change the new module usage

--- ai/vitis.py
+++ ai/vitis.py
@@ -1,11 +1,9 @@
 import sys, os, time, copy, logging
 from flask import Blueprint, current_app
 sys.path.append(os.getcwd())
-try:
-    from ivit_i.utils.draw_tools import Draw as vitis_draw
-    from ivit_i.utils.draw_tools import get_palette
-except Exception as e:
-    logging.error(e)
+from ivit_i.common import api
+from ivit_i.utils.err_handler import handle_exception
+
 # ------------------------------------------------------------------------
 # Vitis-AI
 """ 初始化 Vitis-AI """
@@ -17,55 +15,10 @@ def vitis_init(prim_conf, first_frame=None):
         if "cls" in prim_conf['tag']:
             from ivit_i.cls.classification import Classification as trg
         
-        trg = trg(prim_conf)
-        draw = vitis_draw()
-        palette = get_palette(prim_conf)
+        trg = api.get(prim_conf)
+        trg.load_model(prim_conf, 1)
+        trg.set_async_mode()
 
-        trg.load_model()
-
-        runtime, palette = "Temp", palette
-    
     except Exception as e:
-        trg = e
-        logging.error(e)
-        runtime, palette, draw = None, None, None
-
-    return trg, runtime, draw, palette
-
-""" 進行 AI 辨識 """
-def vitis_inference(frame, uuid, model_conf, trg, runtime, draw, palette, ret_draw=True):
-    
-    ret, info = False, None
-    
-    try:
-        info = trg.inference(frame)
-            
-        if info is not None:
-                
-                if ret_draw:
-                    info.update( { "frame": frame} )
-                    frame = draw.draw_detections(info, palette, model_conf)
-                ret = True
-                info.pop('frame', None) # original
-                info.pop('output_transform', None)    
-                
-                if not ('detections' in info.keys()):
-                    logging.error('unexcepted key {}, should be "detections" in Vitis-AI'.format(info.keys()))
-                else:
-                    logging.debug('parse the results')
-                    
-                    for idx, det in enumerate(info['detections']): 
-                        if model_conf['tag'] in ['cls', 'obj']:
-                            # convert bounding box from float to int
-                            for key in ['xmin', 'xmax', 'ymin', 'ymax']:
-                                det[key] = int(float(det[key])) if det[key]!=None else det[key]
-                            info['detections'][idx]=det
-                        else:
-                            logging.debug('not classification and object detection')
-    
-    except Exception as e:
-        ret = False
-        logging.error(e)
-
-    return ret, info, frame
-   
+        raise Exception(handle_exception(e))
+    return trg
