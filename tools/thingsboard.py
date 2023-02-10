9bc92272c19b777cb8493bd00760e70f203bfaa1
MaxChangInnodisk <max_chang@innodisk.com>
2023-02-10 10:10:58 +0800

    refact: new usage of the post and get api

--- tools/thingsboard.py
+++ tools/thingsboard.py
@@ -21,49 +21,64 @@ HEAD_MAP = {
     "json": {'Content-type': 'application/json'}
 }
 
-def post_api(tb_url, data, h_type='json', timeout=10, stderr=True):
+def response_status(code):
+    """ Return the response is success or not """
+    return (str(code)[0] not in [ '4', '5' ])
+
+
+def send_post_api(tb_url, data, h_type='json', timeout=10, stderr=True):
     """ Using request to simulate POST method """
-    headers     = HEAD_MAP[h_type]
     
+    headers = HEAD_MAP[h_type]
+
     try:
         resp = requests.post(tb_url, data=json.dumps(data), headers=headers, timeout=timeout)
         code = resp.status_code
+        data = { "status": code }
 
         # Convert Json
-        try: data = json.loads(resp.text)
-        except Exception as e: data = resp.text
-        print(code, resp)
-        return code, { 
-            "data": data,
-            "status": code
-        }
+        try: resp_data = json.loads(resp.text)
+        except Exception as e: resp_data = { "data": resp.text }
+        
+        # Update content of the response into data variable
+        data.update(resp_data)
+        return response_status(code), data
 
     except requests.Timeout:
-        return 400, "Request Time Out !!! ({})".format(tb_url)
+        return False, { "data": f"Request Time Out !!! ({tb_url})", "status": 400 }
 
     except requests.ConnectionError:
-        return 400, "Connect Error !!! ({})".format(tb_url)
+        return False, { "data": f"Connect Error !!! ({tb_url})", "status": 400 }
+
+    except Exception as e: 
+        return False, { "data": f"Unxepected Error !!! ({handle_exception(e)})", "status": 400 }
     
-def get_api(tb_url, h_type='json', timeout=10):
+def send_get_api(tb_url, h_type='json', timeout=10):
     """ Using request to simulate GET method """
+
     headers     = HEAD_MAP[h_type]
 
     try:
         resp = requests.get(tb_url, headers=headers, timeout=10)
         code = resp.status_code
-        
+        data = { "status": code }
+
         # Convert Json
-        try: data = json.loads(resp.text)
-        except Exception as e: data = resp.text
+        try: resp_data = json.loads(resp.text)
+        except Exception as e: resp_data = { "data": resp.text }
         
-        # Update Status Code into Response
-        return code, { 
-            "data": data,
-            "status": code
-        }
-        
+        # Update content of the response into data variable
+        data.update(resp_data)
+        return response_status(code), data
+    
+    except requests.Timeout:
+        return False, { "data": f"Request Time Out !!! ({tb_url})", "status": 400 }
+
+    except requests.ConnectionError:
+        return False, { "data": f"Connect Error !!! ({tb_url})", "status": 400 }
+    
     except Exception as e: 
-        return 400, { "data": handle_exception(e) }
+        return False, { "data": f"Unxepected Error !!! ({handle_exception(e)})", "status": 400 }
     
 def register_tb_device(tb_url):
     """
