8f5b7f66055ea48f40d0556104dd5f9464069074
MaxChangInnodisk <max_chang@innodisk.com>
2023-02-13 15:32:49 +0800

    feat: move several basic parameters to config.py from ivit-i.json

--- api/config.py
+++ api/config.py
@@ -4,21 +4,27 @@ class config(object):
     JSON_SORT_KEYS              = False
     TASK_ROOT       = "./task"
     TASK_CFG_NAME   = "task.json"
+    DEBUG           = False
+
+    PLATFORM        = "intel"
+    FRAMEWORK       = "openvino"
+
     TASK            = dict()
     TASK_LIST       = dict()
     APPLICATION     = dict()
     UUID            = dict()
-    RE_UUID         = dict()
+    
     SRC             = dict()
     ALLOWED_HOSTS   = ['*']
-    RE_SRC          = '0'
-    SOCK_POOL            = dict()
+    SOCK_POOL       = dict()
 
     MQTT_BROKER_URL = ""
     MQTT_USERNAME   = ""
     MQTT_PASSWORD   = ""
-    MQTT_BROKER_PORT   = 1883
-    
+    MQTT_BROKER_PORT    = 1883
+    MQTT_KEEPALIVE      = 5
+    MQTT_REFRESH_TIME   = 1.0
+
     TB_API_REG_DEVICE   = "/api/v1/devices"
     TB_TOPIC_REC_RPC    = "v1/devices/me/rpc/request/"
     TB_TOPIC_SND_RPC    = "v1/devices/me/rpc/response/"
