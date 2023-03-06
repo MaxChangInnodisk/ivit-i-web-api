6838df716692f1b5c1e44f0e8da58238c5324ef7
MaxChangInnodisk <max_chang@innodisk.com>
2023-03-06 14:41:23 +0800

    feat: add http_msg

--- api/application.py
+++ api/application.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, current_app
 from flasgger import swag_from
 from .common import get_request_data
+from ..tools.common import http_msg
 
 YAML_PATH   = "../docs/application"
 BP_NAME     = "application"
@@ -15,28 +16,28 @@ APP_DIR     = "APP_DIR"
 @bp_application.route("/tag_app", methods=["GET"] )
 @swag_from("{}/{}".format(YAML_PATH, "tag_app.yml"))
 def tag_app():
-    return jsonify( current_app.config[TAG_APP] )
+    return http_msg( current_app.config[TAG_APP], 200 )
 
 @bp_application.route("/model_app", methods=["GET"] )
 @swag_from("{}/{}".format(YAML_PATH, "model_app.yml"))
 def model_app():
-    return jsonify( current_app.config[MODEL_APP] )
+    return http_msg( current_app.config[MODEL_APP], 200 )
 
 @bp_application.route("/app_model", methods=["GET"] )
 @swag_from("{}/{}".format(YAML_PATH, "app_model.yml"))
 def app_model():
-    return jsonify( current_app.config[APP_MODEL] )
+    return http_msg( current_app.config[APP_MODEL], 200 )
 
 @bp_application.route("/get_all_app", methods=["GET"] )
 def get_all_app():
-    return jsonify( list(current_app.config[APP_CTRL].get_all_apps().keys()) ), 200
+    return http_msg( list(current_app.config[APP_CTRL].get_all_apps().keys()), 200 )
 
 @bp_application.route("/get_sort_app", methods=["GET"] )
 def get_sort_app():
-    return jsonify( current_app.config[APP_CTRL].get_sort_apps() ), 200
+    return http_msg( current_app.config[APP_CTRL].get_sort_apps(), 200 )
 
 @bp_application.route("/register_app_folder", methods=["GET"] )
 def register_app():
     with current_app.app_context():
         current_app.config[APP_CTRL].register_from_folder( current_app.config[APP_DIR] )
-    return jsonify( current_app.config[APP_CTRL].get_sort_apps() ), 200
+    return http_msg( current_app.config[APP_CTRL].get_sort_apps(), 200 )
