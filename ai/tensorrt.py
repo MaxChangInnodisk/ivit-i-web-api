7a38eefc809c08c13a1b16e2041e27a0eb43ca41
MaxChangInnodisk <max_chang@innodisk.com>
2023-03-29 03:41:19 +0000

    feat: raise error with exception

--- ai/tensorrt.py
+++ ai/tensorrt.py
@@ -1,5 +1,6 @@
 import logging, sys, os
 from ivit_i.common import api
+from ivit_i.utils import handle_exception
 
 def trt_init(model_conf, first_frame=None):
     logging.info('Init TensorRT Engine')
@@ -7,5 +8,5 @@ def trt_init(model_conf, first_frame=None):
         trg = api.get(model_conf)
         trg.load_model(model_conf)
     except Exception as e:
-        raise Exception('Could not load AI Model')
+        raise Exception('Could not load AI Model {}'.format(handle_exception(e)))
     return trg
